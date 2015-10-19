69eb4094c4fe035158acfefef44a6bacd488404a
isilanes <isilanes@gmail.com>
2015-10-19 21:01:49 +0200

    from board to Graph object, done

--- libvinidium/graph.py
+++ libvinidium/graph.py
@@ -5,45 +5,69 @@ board = '##@1    ####    @4##      ########              ####            []     
 class Tile(object):
     """Any tile is a node in a graph."""
 
-    def __init__(self, pos):
-        self.pos = pos # (x,y) tuple
+    def __init__(self, pos_x, pos_y, tile_type):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.type = tile_type
+
+    def __repr__(self):
+        return self.type
 
 
 class Edge(object):
     """An Edge object connects two Tile objects."""
 
-    def __init__(self):
-        self.origin = None
-        self.destiny = None
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+
+    def __repr__(self):
+        return '({0.pos_x}, {0.pos_y}) --> ({1.pos_x}, {1.pos_y})'.format(self.src, self.dest)
 
 
 class MapGraph(object):
     """The whole map, as a directed graph."""
 
     def __init__(self):
-        pass
+        self.tile_array = []
+        self.edges = {}
 
     def eat(self, board):
         """Read board string and populate graph with Tile nodes and Edges."""
 
         size = int(math.sqrt(len(board)/2))
         
-        i, j = 0, 0
-        while board:
-            print i,j
-            this_tile, board = board[:2], board[2:]
-            if this_tile == "  " or this_tile[0] == "@":
-                # Passable terrain. We can get into them, and from them
-                # into surrounding ones:
-                pass
-            elif this_tile  == "[]" or this_tile[0] == "$":
-                # Taverns and mines can be "accessed", but not moved from
-                # them to somewhere else:
-                pass
-            j += 1
-            if j >= size:
-                j = 0
-                i += 1
+        # Build array:
+        for i in range(size):
+            col = []
+            for j in range(size):
+                tile_code, board = board[:2], board[2:]
+                this_tile = Tile(i, j, tile_code)
+                self.edges[(i,j)] = []
+                col.append(this_tile)
+            self.tile_array.append(col)
+
+        # From Tile array, build Edge list:
+        for i in range(size):
+            for j in range(size):
+                this_tile = self.tile_array[i][j]
+                if this_tile.type == "  " or this_tile.type[0] == "@":
+                    # Only passable terrain and tiles with a hero (hence,
+                    # passable), have outgoing edges:
+                    for di,dj in [ (-1,0), (0,1), (1,0), (0,-1) ]: # up, right, down, left
+                        if i + di < 0 or j + dj < 0:
+                            continue
+                        try:
+                            other_tile = self.tile_array[i+di][j+dj]
+                        except IndexError:
+                            # other_tile is outside board
+                            continue
+                        if other_tile.type != "##": # only impassable tiles have no inbound Edges
+                            E = Edge(this_tile, other_tile)
+                            self.edges[(i,j)].append(E)
+
+        for col in self.tile_array:
+            print col
 
 
 M = MapGraph()
