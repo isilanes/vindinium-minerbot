c15c7ae0b0fd51766cec4d988fb1232ae6e9c867
Iñaki Silanes <isilanes@gmail.com>
2015-10-21 15:35:31 +0200

    cleanup of unused code

--- libvinidium/game.py
+++ libvinidium/game.py
@@ -1,31 +1,5 @@
 from libvinidium import graph
 
-TAVERN = 0
-AIR = -1
-WALL = -2
-
-PLAYER1 = 1
-PLAYER2 = 2
-PLAYER3 = 3
-PLAYER4 = 4
-
-AIM = {'North': (-1, 0),
-       'East': (0, 1),
-       'South': (1, 0),
-       'West': (0, -1)}
-
-class HeroTile(object):
-
-    def __init__(self, id):
-        self.id = id
-
-
-class MineTile(object):
-
-    def __init__(self, heroId = None):
-        self.heroId = heroId
-
-
 class Game(object):
     """Take state JSON object and build a whole "game-status" object."""
 
