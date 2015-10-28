85885f951464de86e7b24af958a9b87a4d641049
Iñaki Silanes <isilanes@gmail.com>
2015-10-28 17:13:17 +0100

    plot.py: plot running mean too

--- libvindinium/plot.py
+++ libvindinium/plot.py
@@ -1,5 +1,6 @@
 import os
 import pylab
+import numpy
 import argparse
 
 #------------------------------------------------------------------------------#
@@ -19,14 +20,17 @@ fn = os.path.join(os.environ["HOME"], '.vindinium', '{0}.elo'.format(o.user_id))
 
 # Read data:
 X = []
+A = []
 with open(fn) as f:
     for line in f:
         date, time, elo = line.split()
-        X.append(elo)
+        X.append(int(elo))
+        A.append(numpy.mean(X))
 
 # Plot:
 pylab.figure(figsize=(10,6), dpi=120)
-pylab.plot(X, "bo-")
+pylab.plot(X, "bo--")
+pylab.plot(A, "g-")
 pylab.xlabel("# partida")
 pylab.ylabel("Elo")
 pylab.show()
